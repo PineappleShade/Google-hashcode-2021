c2aa2a47a575ab49bdafb986c274e0f4a5157a68
Mohamed Cherifi <mohamed.cherifi@simthetiq.com>
2021-02-25 13:39:01 -0500

    wip

---
+++ Google-hashcode-2021/Path.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Google_hashcode_2021
+{
+    class Path
+    {
+        private int nbrOfStreetToTravel;
+        private string[] streetsPath;
+
+        public int NbrOfStreetToTravel
+        {
+            get => nbrOfStreetToTravel;
+            set => nbrOfStreetToTravel = value;
+        }
+
+        public string[] StreetsPath
+        {
+            get => streetsPath;
+            set => streetsPath = value;
+        }
+    }
+}
