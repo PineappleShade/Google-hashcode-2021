f7554fee0586220007566722519c6be9e056ebd1
Mohamed Cherifi <mohamed.cherifi@simthetiq.com>
2021-02-25 13:32:05 -0500

    wip

---
+++ Google-hashcode-2021/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Google_hashcode_2021
+{
+    class Street
+    {
+        private int startingIntersection;
+        private int endIntersection;
+        private string streetName;
+        private int L;
+
+        public int StartingIntersection
+        {
+            get => startingIntersection;
+            set => startingIntersection = value;
+        }
+
+        public int EndIntersection
+        {
+            get => endIntersection;
+            set => endIntersection = value;
+        }
+
+        public string StreetName
+        {
+            get => streetName;
+            set => streetName = value;
+        }
+
+        public int L1
+        {
+            get => L;
+            set => L = value;
+        }
+    }
+}
