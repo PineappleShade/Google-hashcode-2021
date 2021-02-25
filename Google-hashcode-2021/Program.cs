f200c9d5d6698a31285635165f5ee22fb28d8861
Yao Hua Shan <yh.shan97@outlook.com>
2021-02-25 00:22:16 -0500

    ReadFile test

--- Google-hashcode-2021/Program.cs
+++ Google-hashcode-2021/Program.cs
@@ -8,6 +8,28 @@ namespace Google_hashcode_2021
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hey br");
+
+            Console.WriteLine(ReadFile(a_example.txt));
+
+
+        }
+
+
+        public ArrayList ReadFile(string filePath)
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader(filePath);
+            ArrayList tabString = new ArrayList();
+            int counter = 0;
+
+
+            while ((line = file.ReadLine()) != null)
+            {
+                tabString.add(line);
+
+                counter++;
+            }
+
+            return tabString;
         }
     }
 }
